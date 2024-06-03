f9a1b26bb037c6c915e1f6e58814d59be50e7268
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-03 17:08:39 +0200

    Funcionality added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const start = document.querySelector(".start");
 const board = document.querySelector(".board");
+const treasure = document.querySelector(".treasure");
+const click = document.querySelector(".this");
 
 start.addEventListener("click", function() {
     if(start) {
@@ -7,3 +9,11 @@ start.addEventListener("click", function() {
         start.style.display = "none";
     }
 });
+
+let buttons = document.querySelectorAll(".this")
+for( let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        treasure.style.display = "block";
+        click.style.display = "none";
+    })
+}
